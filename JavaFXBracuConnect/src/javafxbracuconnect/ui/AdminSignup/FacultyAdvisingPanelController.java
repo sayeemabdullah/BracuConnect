6ba3a18505cee65c0cb787697dd652139f3adb36
Sayeem Abdullah <Sayeem Abdullah@DESKTOP-P9KKGMR>
2019-07-12 20:17:37 +0600

    version 1.7

--- JavaFXBracuConnect/src/javafxbracuconnect/ui/AdminSignup/FacultyAdvisingPanelController.java
+++ JavaFXBracuConnect/src/javafxbracuconnect/ui/AdminSignup/FacultyAdvisingPanelController.java
@@ -9,11 +9,15 @@ import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -24,6 +28,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 /**
@@ -32,21 +37,22 @@ import javafx.stage.Stage;
  * @author Sayeem Abdullah
  */
 public class FacultyAdvisingPanelController implements Initializable {
+    ObservableList <AdvisingStudent> alist = FXCollections.observableArrayList();
 
     @FXML
-    private TableView<?> allstudentcoursetable;
+    private TableView<AdvisingStudent> allstudentcoursetable;
     @FXML
-    private TableColumn<?, ?> id;
+    private TableColumn<AdvisingStudent,String> id;
     @FXML
-    private TableColumn<?, ?> c1;
+    private TableColumn<AdvisingStudent,String> c1;
     @FXML
-    private TableColumn<?, ?> c2;
+    private TableColumn<AdvisingStudent,String> c2;
     @FXML
-    private TableColumn<?, ?> c3;
+    private TableColumn<AdvisingStudent,String> c3;
     @FXML
-    private TableColumn<?, ?> c4;
+    private TableColumn<AdvisingStudent,String> c4;
     @FXML
-    private TableColumn<?, ?> c5;
+    private TableColumn<AdvisingStudent,String> c5;
     @FXML
     private Button advising;
     @FXML
@@ -65,8 +71,49 @@ public class FacultyAdvisingPanelController implements Initializable {
         } catch (SQLException ex) {
             Logger.getLogger(FacultyAdvisingPanelController.class.getName()).log(Level.SEVERE, null, ex);
         }
+        initCol();
+        try {
+            AdvisingStudentloadData();
+        } catch (SQLException ex) {
+            Logger.getLogger(FacultyAdvisingPanelController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }    
+     private void initCol() {
+        id.setCellValueFactory(new PropertyValueFactory<>("sid"));
+        c1.setCellValueFactory(new PropertyValueFactory<>("sc1"));
+        c2.setCellValueFactory(new PropertyValueFactory<>("sc2"));
+        c3.setCellValueFactory(new PropertyValueFactory<>("sc3"));
+        c4.setCellValueFactory(new PropertyValueFactory<>("sc4"));
+        c5.setCellValueFactory(new PropertyValueFactory<>("sc5"));
+    }
+     
+     private void AdvisingStudentloadData() throws SQLException {
+        
+        String qu = "SELECT * FROM SAYEEM.ADVISEDCOURSE ";
+        System.out.println(qu);
+        con = DriverManager.getConnection("jdbc:derby://localhost:1527/BracuConnect", "sayeem", "17101009");
+        stm = con.createStatement();
+        ResultSet rest = stm.executeQuery(qu);
+        
+        try {
+            while (rest.next()) {
+                String stid = rest.getString("id");
+                String stc1 = rest.getString("course1");
+                String stc2 = rest.getString("course2");
+                String stc3 = rest.getString("course3");
+                String stc4 = rest.getString("course4");
+                String stc5 = rest.getString("course5");
+                
+                
+                FacultyAdvisingPanelController.AdvisingStudent advisingstudent = new FacultyAdvisingPanelController.AdvisingStudent (stid,stc1,stc2,stc3,stc4,stc5);
+                alist.add(advisingstudent);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(FacultyAdvisingPanelController.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
+        allstudentcoursetable.getItems().setAll(alist);
+    }
     @FXML
     private void gotoadvising(ActionEvent event) throws IOException {
         Parent adminDash = FXMLLoader.load(getClass().getResource("FacultyStudentAdvising.fxml"));
@@ -86,5 +133,44 @@ public class FacultyAdvisingPanelController implements Initializable {
         window.setScene(adminDashScene);
         window.show();
     }
+    
+    public class AdvisingStudent {
+        private final SimpleStringProperty sid;
+        private final SimpleStringProperty sc1;
+        private final SimpleStringProperty sc2;
+        private final SimpleStringProperty sc3;
+        private final SimpleStringProperty sc4;
+        private final SimpleStringProperty sc5;
+        
+
+        public AdvisingStudent(String sid , String sc1, String sc2, String sc3, String sc4, String sc5) {
+            this.sid = new SimpleStringProperty(sid);
+            this.sc1 = new SimpleStringProperty(sc1);
+            this.sc2 = new SimpleStringProperty(sc2);
+            this.sc3 = new SimpleStringProperty(sc3);
+            this.sc4 = new SimpleStringProperty(sc4);
+            this.sc5 = new SimpleStringProperty(sc5);
+            
+        }
+
+        public String getid() {
+            return sid.get();
+        }
+        public String getC1() {
+            return sc1.get();
+        }
+        public String getC2() {
+            return sc2.get();
+        }
+        public String getC3() {
+            return sc3.get();
+        }
+        public String getC4() {
+            return sc4.get();
+        }
+        public String getC5() {
+            return sc5.get();
+        }
+        }
     
 }
