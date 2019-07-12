54f345d837eb30feb7061df3a2bd52a525a3c3fc
Sayeem Abdullah <Sayeem Abdullah@DESKTOP-P9KKGMR>
2019-07-12 06:26:46 +0600

    version 1.6

--- JavaFXBracuConnect/src/javafxbracuconnect/ui/AdminSignup/FacultyStudentAdvisingController.java
+++ JavaFXBracuConnect/src/javafxbracuconnect/ui/AdminSignup/FacultyStudentAdvisingController.java
@@ -9,11 +9,16 @@ import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -21,11 +26,13 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 /**
@@ -34,31 +41,32 @@ import javafx.stage.Stage;
  * @author Sayeem Abdullah
  */
 public class FacultyStudentAdvisingController implements Initializable {
-
+    ObservableList <Student> slist = FXCollections.observableArrayList();
+    ObservableList <AdvisingStudent> alist = FXCollections.observableArrayList();
     @FXML
     private TextField id;
     @FXML
-    private ComboBox<?> c1;
+    private ComboBox<String> c1;
     @FXML
-    private ComboBox<?> c2;
+    private ComboBox<String> c2;
     @FXML
-    private ComboBox<?> c3;
+    private ComboBox<String> c3;
     @FXML
-    private ComboBox<?> c4;
+    private ComboBox<String> c4;
     @FXML
-    private ComboBox<?> c5;
+    private ComboBox<String> c5;
     @FXML
     private Button updateadvising;
     @FXML
-    private TableView<?> studentinfo;
+    private TableView<Student> studentinfo;
     @FXML
-    private TableColumn<?, ?> name;
+    private TableColumn<Student,String> name;
     @FXML
-    private TableColumn<?, ?> dept;
+    private TableColumn<Student,String> dept;
     @FXML
-    private TableColumn<?, ?> number;
+    private TableColumn<Student,String> number;
     @FXML
-    private TableColumn<?, ?> email;
+    private TableColumn<Student,String> email;
     @FXML
     private TextField studentid;
     @FXML
@@ -66,22 +74,26 @@ public class FacultyStudentAdvisingController implements Initializable {
     @FXML
     private Button refresh;
     @FXML
-    private TableView<?> courseinfo;
+    private TableView<AdvisingStudent> courseinfo;
     @FXML
-    private TableColumn<?, ?> co1;
+    private TableColumn<AdvisingStudent,String> co1;
     @FXML
-    private TableColumn<?, ?> co2;
+    private TableColumn<AdvisingStudent,String> co2;
     @FXML
-    private TableColumn<?, ?> co3;
+    private TableColumn<AdvisingStudent,String> co3;
     @FXML
-    private TableColumn<?, ?> co4;
+    private TableColumn<AdvisingStudent,String> co4;
     @FXML
-    private TableColumn<?, ?> co5;
+    private TableColumn<AdvisingStudent,String> co5;
     @FXML
     private Button facultydash;
     Connection con;
     Statement stm;
+    ResultSet re; 
     int res;
+    String SID;
+    ResultSet rest;
+    ResultSet rest2;
     /**
      * Initializes the controller class.
      */
@@ -92,9 +104,78 @@ public class FacultyStudentAdvisingController implements Initializable {
         } catch (SQLException ex) {
             Logger.getLogger(FacultyStudentAdvisingController.class.getName()).log(Level.SEVERE, null, ex);
         }
-
+        try {
+            fillComboBox();
+        } catch (SQLException ex) {
+            Logger.getLogger(FacultyStudentAdvisingController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        initCol();
     }    
+    private void initCol() {
+        name.setCellValueFactory(new PropertyValueFactory<>("sname"));
+        dept.setCellValueFactory(new PropertyValueFactory<>("sdept"));
+        email.setCellValueFactory(new PropertyValueFactory<>("semail"));
+        number.setCellValueFactory(new PropertyValueFactory<>("snumber"));
+        co1.setCellValueFactory(new PropertyValueFactory<>("sc1"));
+        co2.setCellValueFactory(new PropertyValueFactory<>("sc2"));
+        co3.setCellValueFactory(new PropertyValueFactory<>("sc3"));
+        co4.setCellValueFactory(new PropertyValueFactory<>("sc4"));
+        co5.setCellValueFactory(new PropertyValueFactory<>("sc5"));
+    }
+    
+    private void StudentloadData() throws SQLException {
+        
+        String qu = "SELECT NAME , DEPARTMENT , EMAIL , PHONENUMBER  FROM SAYEEM.STUDENT WHERE ID = '" + SID + "'";
+        con = DriverManager.getConnection("jdbc:derby://localhost:1527/BracuConnect", "sayeem", "17101009");
+        stm = con.createStatement();
+        rest = stm.executeQuery(qu);
+        
+        try {
+            while (rest.next()) {
+                String stname = rest.getString("name");
+                String stdept = rest.getString("department");
+                String stemail = rest.getString("email");
+                String stnumber = rest.getString("phonenumber");
 
+                
+                
+                FacultyStudentAdvisingController.Student student = new FacultyStudentAdvisingController.Student (stname,stdept,stemail,stnumber);
+                slist.add(student);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(FacultyStudentAdvisingController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        studentinfo.getItems().setAll(slist);
+    }
+    
+    private void AdvisingStudentloadData() throws SQLException {
+        
+        String qu = "SELECT COURSE1 , COURSE2 , COURSE3 , COURSE4 , COURSE5  FROM SAYEEM.ADVISEDCOURSE WHERE ID = '" + SID + "'";
+        System.out.println(qu);
+        con = DriverManager.getConnection("jdbc:derby://localhost:1527/BracuConnect", "sayeem", "17101009");
+        stm = con.createStatement();
+        rest2 = stm.executeQuery(qu);
+        
+        try {
+            while (rest2.next()) {
+                String stc1 = rest2.getString("course1");
+                String stc2 = rest2.getString("course2");
+                String stc3 = rest2.getString("course3");
+                String stc4 = rest2.getString("course4");
+                String stc5 = rest2.getString("course5");
+                
+                
+                FacultyStudentAdvisingController.AdvisingStudent advisingstudent = new FacultyStudentAdvisingController.AdvisingStudent (stc1,stc2,stc3,stc4,stc5);
+                alist.add(advisingstudent);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(FacultyStudentAdvisingController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        courseinfo.getItems().setAll(alist);
+    }
+    
     @FXML
     private void getid(ActionEvent event) {
     }
@@ -120,7 +201,101 @@ public class FacultyStudentAdvisingController implements Initializable {
     }
 
     @FXML
-    private void doneadvising(ActionEvent event) {
+    private void doneadvising(ActionEvent event) throws SQLException, IOException {
+        String StudentID = id.getText();
+        System.out.println(StudentID);
+        String DuplicateChecker = "SELECT * FROM SAYEEM.STUDENT WHERE ID = '" + StudentID + "'";
+        PreparedStatement pst = con.prepareStatement(DuplicateChecker);
+        System.out.println(DuplicateChecker);
+        ResultSet rs = pst.executeQuery();
+        if (rs.next()) {    
+            
+        }
+        else{
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setHeaderText(null);
+            alert.setContentText("Student ID doesn't exists.");
+            alert.showAndWait();
+            return;
+
+        }
+
+        String C01 = c1.getValue();
+        if(C01==null){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setHeaderText(null);
+            alert.setContentText("Please Choose the courses you want to advise.");
+            alert.showAndWait();
+            return;
+        }
+        String C02 = c2.getValue();
+        if((C01!=null && C02!=null) && C01.equals(C02)){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setHeaderText(null);
+            alert.setContentText("Please Choose Different Courses. You Cannot advise the same course twice.");
+            alert.showAndWait();
+            return;
+        }
+        String C03 = c3.getValue();
+        if((C01!=null && C02!=null && C03!=null) && ((C01.equals(C03)) || (C02.equals(C03)))){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setHeaderText(null);
+            alert.setContentText("Please Choose Different Courses. You Cannot advise the same course twice.");
+            alert.showAndWait();
+            return;
+        }
+        String C04 = c4.getValue();
+         if((C01!=null && C02!=null && C03!=null && C04!=null) && ((C01.equals(C04)) || (C02.equals(C04)) || (C03.equals(C04)))){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setHeaderText(null);
+            alert.setContentText("Please Choose Different Courses. You Cannot advise the same course twice.");
+            alert.showAndWait();
+            return;
+        }
+        String C05 = c5.getValue();
+        if((C01!=null && C02!=null && C03!=null && C04!=null && C05!=null) && ((C01.equals(C05)) || (C02.equals(C05)) || (C03.equals(C05))|| (C04.equals(C05)))){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setHeaderText(null);
+            alert.setContentText("Please Choose Different Courses. You Cannot advise the same course twice.");
+            alert.showAndWait();
+            return;
+        }
+        
+        String qu = "INSERT INTO ADVISEDCOURSE VALUES("
+                + "'" + StudentID + "',"
+                + "'" + C01 + "',"
+                + "'" + C02 + "',"
+                + "'" + C03 + "',"
+                + "'" + C04 + "',"
+                + "'" + C05 + "'"
+                + ")";
+        con = DriverManager.getConnection("jdbc:derby://localhost:1527/BracuConnect", "sayeem", "17101009");
+        stm = con.createStatement();
+        res = stm.executeUpdate(qu);
+         if (res != 0) {
+                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                alert.setHeaderText(null);
+                alert.setContentText("Advising Successful");
+                alert.showAndWait();
+                Parent ssignin = FXMLLoader.load(getClass().getResource("FacultyStudentAdvising.fxml"));
+                Scene ssigninScene = new Scene(ssignin);
+                Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                window.setTitle("BracuConnect");
+                window.setScene(ssigninScene);
+                window.show();
+        }
+         else{
+             Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setHeaderText(null);
+                alert.setContentText("Advising Unsuccessful.Please update advising as advising was done once.");
+                alert.showAndWait();
+                Parent ssignin = FXMLLoader.load(getClass().getResource("FacultyStudentAdvising.fxml"));
+                Scene ssigninScene = new Scene(ssignin);
+                Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                window.setTitle("BracuConnect");
+                window.setScene(ssigninScene);
+                window.show();
+         }
     }
 
     @FXML
@@ -128,7 +303,32 @@ public class FacultyStudentAdvisingController implements Initializable {
     }
 
     @FXML
-    private void loadbystudentid(ActionEvent event) {
+    private void loadbystudentid(ActionEvent event) throws SQLException {
+        SID = studentid.getText();
+        if ( SID.isEmpty()) {
+               Alert alert = new Alert(Alert.AlertType.ERROR);
+               alert.setHeaderText(null);
+               alert.setContentText("Please Enter Student ID");
+               alert.showAndWait();
+               return;
+           }
+        String DuplicateChecker = "SELECT * FROM SAYEEM.STUDENT WHERE ID = '" + SID + "'";
+           PreparedStatement pst = con.prepareStatement(DuplicateChecker);
+           System.out.println(DuplicateChecker);
+           ResultSet rs = pst.executeQuery();
+           if (rs.next()) {
+               System.out.println("ok");
+           }
+           else{
+           Alert alert = new Alert(Alert.AlertType.ERROR);
+               alert.setHeaderText(null);
+               alert.setContentText("Please Enter Existing Student's ID");
+               alert.showAndWait();
+               return;
+           }
+           
+           AdvisingStudentloadData();
+           StudentloadData();
     }
 
     @FXML
@@ -141,8 +341,8 @@ public class FacultyStudentAdvisingController implements Initializable {
         window.show();
     }
 
-    @FXML
-    private void gotofacultydash(ActionEvent event) throws IOException { // GO TO FACULTY ADVISING PANEL
+    @FXML // GO TO FACULTY ADVISING PANEL
+    private void gotofacultydash(ActionEvent event) throws IOException { 
         Parent adminDash = FXMLLoader.load(getClass().getResource("FacultyAdvisingPanel.fxml"));
         Scene adminDashScene = new Scene(adminDash);
         Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
@@ -151,4 +351,89 @@ public class FacultyStudentAdvisingController implements Initializable {
         window.show();
     }
     
+    public void fillComboBox() throws SQLException{
+        String qu = "SELECT CODE FROM COURSE";
+        con = DriverManager.getConnection("jdbc:derby://localhost:1527/BracuConnect", "sayeem", "17101009");
+        stm = con.createStatement();
+        re = stm.executeQuery(qu);
+        while(re.next()){
+            c1.getItems().add(re.getString("CODE"));
+            c2.getItems().add(re.getString("CODE"));
+            c3.getItems().add(re.getString("CODE"));
+            c4.getItems().add(re.getString("CODE"));
+            c5.getItems().add(re.getString("CODE"));
+        }
+    
+    }
+
+    @FXML
+    private void gotoupdateadvising(ActionEvent event) {
+    }
+    
+     public class Student {
+
+        private final SimpleStringProperty sname;
+        private final SimpleStringProperty sdept;
+        private final SimpleStringProperty semail;
+        private final SimpleStringProperty snumber;
+        
+
+        public Student(String sname, String sdept, String semail, String snumber) {
+            this.sname = new SimpleStringProperty(sname);
+            this.sdept = new SimpleStringProperty(sdept);
+            this.semail = new SimpleStringProperty(semail);
+            this.snumber = new SimpleStringProperty(snumber);
+            
+        }
+
+
+        public String getSname() {
+            return sname.get();
+        }
+        public String getSdept() {
+            return sdept.get();
+        }
+        public String getSemail() {
+            return semail.get();
+        }
+        public String getSnumber() {
+            return snumber.get();
+        }
+        
+        }
+     public class AdvisingStudent {
+
+        private final SimpleStringProperty sc1;
+        private final SimpleStringProperty sc2;
+        private final SimpleStringProperty sc3;
+        private final SimpleStringProperty sc4;
+        private final SimpleStringProperty sc5;
+        
+
+        public AdvisingStudent(String sc1, String sc2, String sc3, String sc4, String sc5) {
+            this.sc1 = new SimpleStringProperty(sc1);
+            this.sc2 = new SimpleStringProperty(sc2);
+            this.sc3 = new SimpleStringProperty(sc3);
+            this.sc4 = new SimpleStringProperty(sc4);
+            this.sc5 = new SimpleStringProperty(sc5);
+            
+        }
+
+
+        public String getC1() {
+            return sc1.get();
+        }
+        public String getC2() {
+            return sc2.get();
+        }
+        public String getC3() {
+            return sc3.get();
+        }
+        public String getC4() {
+            return sc4.get();
+        }
+        public String getC5() {
+            return sc5.get();
+        }
+        }
 }
